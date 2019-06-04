8b0ed5055ecbc6c2c317abf707c23976ed3e3172
ru63biy <d.steidl@campus.lmu.de>
2019-06-04 17:15:57 +0200

    Implement initial qr code scanner

--- tree-app-android/app/src/main/java/de/lmu/treeapp/MainActivity.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/MainActivity.java
@@ -1,20 +1,34 @@
 package de.lmu.treeapp;
 
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import de.lmu.treeapp.barcode.BarcodeCaptureActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int BARCODE_READER_REQUEST_CODE = 1;
+    private final String LOG_TAG = "MainActivity";
+    private TextView welcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FloatingActionButton qrCodeButton = findViewById(R.id.qr_code_button);
+        welcomeTextView = findViewById(R.id.textView);
 
 
         qrCodeButton.setOnClickListener(new Button.OnClickListener() {
@@ -22,7 +36,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast qrToast = Toast.makeText(getApplicationContext(), "QR Code Button clicked", Toast.LENGTH_LONG );
                 qrToast.show();
+
+                Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode =  data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    welcomeTextView.setText(barcode.displayValue);
+                } else
+                    welcomeTextView.setText(R.string.no_barcode_captured);
+            } else
+                Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
+                        CommonStatusCodes.getStatusCodeString(resultCode)));
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
     }
 }
