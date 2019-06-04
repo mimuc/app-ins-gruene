f49c5831a820c30a027ee8ba2afd7016e6c20264
ru63biy <d.steidl@campus.lmu.de>
2019-06-04 16:27:26 +0200

    Add action button to main activity

--- tree-app-android/app/src/main/java/de/lmu/treeapp/MainActivity.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/MainActivity.java
@@ -1,7 +1,12 @@
 package de.lmu.treeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton qrCodeButton = findViewById(R.id.qr_code_button);
+
+
+        qrCodeButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast qrToast = Toast.makeText(getApplicationContext(), "QR Code Button clicked", Toast.LENGTH_LONG );
+                qrToast.show();
+            }
+        });
     }
 }
