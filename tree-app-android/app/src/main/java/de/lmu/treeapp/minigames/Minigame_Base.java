f6408cae345fdc2cd2c768d6af1e91f139b16837
Yannick Weiss <yannick.weiss@gosheim.net>
2019-07-07 21:32:24 +0200

    Removed Minigames from Database
    Removed Minigames from Database, since they will always be static, and
    reading from the future CMS would then be enough.

---
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/minigames/Minigame_Base.java
@@ -0,0 +1,7 @@
+package de.lmu.treeapp.minigames;
+
+public class Minigame_Base {
+    public int uid;
+    public String name;
+    public String description;
+}
---
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/minigames/components/AnswerOption.java
@@ -0,0 +1,24 @@
+package de.lmu.treeapp.minigames.components;
+
+public class AnswerOption {
+
+    public enum OptionTypes {
+        text, image
+    }
+
+    public OptionTypes type;
+    public String content;
+    public Boolean right;
+
+    AnswerOption(OptionTypes _type, String _content, Boolean _right){
+        type = _type;
+        content = _content;
+        right = _right;
+    }
+    AnswerOption(OptionTypes _type, String _content){
+        type = _type;
+        content = _content;
+        right = false;
+    }
+
+}
