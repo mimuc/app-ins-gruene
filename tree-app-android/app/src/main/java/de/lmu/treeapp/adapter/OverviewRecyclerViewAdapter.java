037197baa9fa4bcd79dbe1690cf836043c2ebbf8
Yannick Weiss <yannick.weiss@gosheim.net>
2019-07-14 00:41:32 +0200

    Adde Tree data to overview

--- tree-app-android/app/src/main/java/de/lmu/treeapp/adapter/OverviewRecyclerViewAdapter.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/adapter/OverviewRecyclerViewAdapter.java
@@ -3,26 +3,44 @@ package de.lmu.treeapp.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.List;
+import java.util.Objects;
 
 import de.lmu.treeapp.R;
+import de.lmu.treeapp.contentClasses.trees.Tree;
 
 public class OverviewRecyclerViewAdapter extends RecyclerView.Adapter<OverviewRecyclerViewAdapter.ViewHolder> {
-    private List<String> treeValues;
+    private List<Tree> treeValues;
+
+    private de.lmu.treeapp.service.FragmentManagerService fragmentManager;
+    private Fragment selectedTreeFragment;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView treeName;
+        public final ImageButton treeImage;
+        public final ImageView treeUnlockedStatus;
         ViewHolder(View v) {
             super(v);
+            treeName = v.findViewById(R.id.overview_single_tree_text);
+            treeImage = v.findViewById(R.id.overview_single_tree_image);
+            treeUnlockedStatus = v.findViewById(R.id.overview_single_tree_state_image);
         }
     }
 
-    public void add(int position, String item) {
+    public void add(int position, Tree item) {
         treeValues.add(position, item);
         notifyItemInserted(position);
     }
@@ -33,8 +51,10 @@ public class OverviewRecyclerViewAdapter extends RecyclerView.Adapter<OverviewRe
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public OverviewRecyclerViewAdapter(List<String> treeData) {
-        treeValues = treeData;
+    public OverviewRecyclerViewAdapter(List<Tree> treeData, de.lmu.treeapp.service.FragmentManagerService fragmentManager, Fragment selectedTreeFragment) {
+        this.treeValues = treeData;
+        this.fragmentManager = fragmentManager;
+        this.selectedTreeFragment = selectedTreeFragment;
     }
 
     // Create new views (invoked by the layout manager)
@@ -51,9 +71,31 @@ public class OverviewRecyclerViewAdapter extends RecyclerView.Adapter<OverviewRe
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
+        Tree tree = treeValues.get(position);
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        final String name = treeValues.get(position);
+        holder.treeName.setText(tree.name);
+        if (tree.changeable.unlocked){
+            // Change to Unlocked-Symbol
+            //holder.treeUnlockedStatus.setImageResource(R.drawable.ic_local_florist_white_24dp);
+        }
+        else if (Math.round(tree.GetGameProgressionPercent(Tree.GameCategories.total)) >= 100){
+            // Set the icon to completed
+        }
+        else {
+            // Change to Locked-Symbol
+            //holder.treeUnlockedStatus.setImageResource(R.drawable.ic_local_florist_white_24dp);
+        }
+        // Set Tree-Image here:
+        holder.treeImage.setImageResource(R.drawable.ic_local_florist_white_24dp);
+        holder.treeImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                fragmentManager.showFragment(selectedTreeFragment);
+            }
+
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -61,4 +103,5 @@ public class OverviewRecyclerViewAdapter extends RecyclerView.Adapter<OverviewRe
     public int getItemCount() {
         return treeValues.size();
     }
+
 }
