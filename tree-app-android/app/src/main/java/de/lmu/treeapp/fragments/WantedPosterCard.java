89787f1750b911481363b5b8d1d5438bc11b3505
Beni <benikles>
2019-07-21 00:24:34 +0200

    steckbrief

---
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/fragments/WantedPosterCard.java
@@ -0,0 +1,62 @@
+package de.lmu.treeapp.fragments;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import de.lmu.treeapp.R;
+
+public class WantedPosterCard {
+
+    public enum CardTypes{
+        //Different layout types of cards can be entered here and handled differently in future
+    }
+
+    private final String header;
+    private final String subHeader;
+    private final Drawable picture;
+    private final String infotext;
+    private final boolean unlocked;
+    private Drawable collapse;
+
+
+    public WantedPosterCard(boolean unlocked, String header, Drawable picture, String infoText, Context context) {
+        this.header = header;
+        this.picture = picture;
+        this.infotext = infoText;
+        this.unlocked = unlocked;
+
+        if(unlocked){
+            //TODO: collapse arrow here to be safe
+            subHeader = "Bereits freigespielt";
+
+        } else {
+            subHeader = "Nicht freigespielt";
+            collapse = context.getDrawable(R.drawable.ic_locked);
+
+        }
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getSubHeader() {
+        return subHeader;
+    }
+
+    public Drawable getPicture() {
+        return picture;
+    }
+
+    public String getInfotext() {
+        return infotext;
+    }
+
+    public boolean isUnlocked() {
+        return unlocked;
+    }
+
+    public Drawable getCollapse() {
+        return collapse;
+    }
+}
