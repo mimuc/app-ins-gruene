4c056db7bfc0af90f95e01dc68fe46b028fccfa3
Beni <benikles>
2019-07-21 00:24:34 +0200

    steckbrief

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/WantedPosterDetailsActivity.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/WantedPosterDetailsActivity.java
@@ -1,16 +1,24 @@
 package de.lmu.treeapp.activities;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.lmu.treeapp.R;
+import de.lmu.treeapp.adapter.WantedPosterCardAdapter;
 import de.lmu.treeapp.contentClasses.trees.Tree;
 import de.lmu.treeapp.contentClasses.trees.TreeProfile;
 import de.lmu.treeapp.contentClasses.trees.TreeProfileCard;
 import de.lmu.treeapp.contentData.DataManager;
+import de.lmu.treeapp.fragments.WantedPosterCard;
 
 public class WantedPosterDetailsActivity extends AppCompatActivity {
 
@@ -27,7 +35,11 @@ public class WantedPosterDetailsActivity extends AppCompatActivity {
         treeProfile = DataManager.getInstance(getApplicationContext()).GetTreeProfile(tree.profileId);
 
         // The following should be replaced by a RecyclerView or something similar for the cards
-        TextView contentPlaceholder = findViewById(R.id.wanted_poster_details_content_placeholder);
+        RecyclerView recyclerView = findViewById(R.id.wanted_poster_recycler_view);
+        WantedPosterCardAdapter adapter = new WantedPosterCardAdapter(getCards());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
         String content = "";
         for (int i = 0; i < treeProfile.cards.size(); i++){
             TreeProfileCard card = treeProfile.cards.get(i);
@@ -36,13 +48,31 @@ public class WantedPosterDetailsActivity extends AppCompatActivity {
                 content += card.content + "\n";
             }
         }
-        contentPlaceholder.setText(content);
 
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             String titlePre = getResources().getString(R.string.wanted_poster_details_title_text);
             getSupportActionBar().setTitle( titlePre + " "+ tree.name);
         }
+    }
+
+    private List<WantedPosterCard> getCards() {
+        List<WantedPosterCard> wantedPosterCards = new ArrayList<>();
+        Drawable picture = getDrawable(R.drawable.ic_ahorn_blatt); //TODO: add correct pictures
+
+        for (int i = 0; i < treeProfile.cards.size(); i++){
+            TreeProfileCard card = treeProfile.cards.get(i);
+            boolean unlocked = card.unlockedBy == Tree.GameCategories.none || tree.GetGameProgressionPercent(card.unlockedBy) > 90;
+            WantedPosterCard posterCard = new WantedPosterCard(unlocked,
+                    card.name,
+                    picture,
+                    card.content,
+                    getBaseContext());
+
+            wantedPosterCards.add(posterCard);
+        }
+
+        return wantedPosterCards;
     }
 
     @Override
