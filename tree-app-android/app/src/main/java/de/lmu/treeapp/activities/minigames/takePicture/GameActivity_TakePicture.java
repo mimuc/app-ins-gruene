b86d422e115618b9f9252fcb5500990a18113562
Yannick Weiss <yannick.weiss@gosheim.net>
2019-10-21 19:35:36 +0200

    Added Taking and Saving of Pictures
    - Added FileProvider
    - Added Generation of File including naming in CMS
    - Added Taking and Saving of Pictures
    - Added a Display showing the Picture before Ending the Minigame
    - Next Step: Showing these pictures in Tree-Profile

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/takePicture/GameActivity_TakePicture.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/takePicture/GameActivity_TakePicture.java
@@ -1,9 +1,23 @@
 package de.lmu.treeapp.activities.minigames.takePicture;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.lmu.treeapp.R;
 import de.lmu.treeapp.activities.minigames.base.GameActivity_Base;
@@ -12,6 +26,10 @@ import de.lmu.treeapp.contentClasses.minigames.Minigame_TakePicture;
 public class GameActivity_TakePicture extends GameActivity_Base {
 
     private Minigame_TakePicture takePictureGame;
+
+    private Button sendButton;
+    private ImageButton previewPicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -20,16 +38,75 @@ public class GameActivity_TakePicture extends GameActivity_Base {
 
 
         takePictureGame = (Minigame_TakePicture) gameContent;
-        Button sendButton = findViewById(R.id.game_takePicture_sendButton);
+        sendButton = findViewById(R.id.game_takePicture_sendButton);
+
+        previewPicture = findViewById(R.id.game_takePicture_previewPicture);
+
+        previewPicture.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         sendButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                // Create Link to file for Tree_Profile etc.
                 onSuccess();
             }
         });
 
     }
+
+    String currentPhotoPath;
+    Uri photoURI;
+
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                 photoURI = FileProvider.getUriForFile(this,
+                        "de.lmu.treeapp.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        String imageFileName = "AppInsGruene_" + takePictureGame.GetPictureName();
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+            previewPicture.setImageURI(photoURI);
+        }
+    }
+
+
+
 }
