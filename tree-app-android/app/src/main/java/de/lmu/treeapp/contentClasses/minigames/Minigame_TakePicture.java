d06356cf6ece9fe2ec4c2b3c14e9fd866385dc65
Yannick Weiss <yannick.weiss@gosheim.net>
2019-10-21 19:35:36 +0200

    Added Taking and Saving of Pictures
    - Added FileProvider
    - Added Generation of File including naming in CMS
    - Added Taking and Saving of Pictures
    - Added a Display showing the Picture before Ending the Minigame
    - Next Step: Showing these pictures in Tree-Profile

--- tree-app-android/app/src/main/java/de/lmu/treeapp/contentClasses/minigames/Minigame_TakePicture.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/contentClasses/minigames/Minigame_TakePicture.java
@@ -7,6 +7,6 @@ public class Minigame_TakePicture extends Minigame_Base {
     public void FillPictureName(String _name){
         pictureName = _name.trim().toLowerCase();
     }
-
+    public String GetPictureName() { return pictureName;}
 
 }
