b67c4caa216e6265c2ad48013d2d321754e1eb63
RauschkugelxD <lisagoertz95@gmx.de>
2020-05-27 19:26:23 +0200

    Improved Quiz Question Forwarding, Creating PopUp test-version

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/chooseAnswer/ChooseAnswer_Options_RecyclerViewAdapter.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/chooseAnswer/ChooseAnswer_Options_RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package de.lmu.treeapp.activities.minigames.chooseAnswer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
