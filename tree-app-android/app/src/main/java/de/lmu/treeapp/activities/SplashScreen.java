389d5cd198be93455a9ad3e98e00ab628847d97c
caraemberger <emberger@cip.ifi.lmu.de>
2020-08-10 22:19:31 +0200

    Splash Screen
    upload of splash screen

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/SplashScreen.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/SplashScreen.java
@@ -3,6 +3,8 @@ package de.lmu.treeapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import de.lmu.treeapp.R;
@@ -14,7 +16,10 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        getSupportActionBar().hide();
+
+        ImageView image_appinsgruene = findViewById(R.id.image_appinsgruene);
+        ImageView image_logos = findViewById(R.id.image_logos);
+        TextView splash_screen_text = findViewById(R.id.splash_screen_text);
 
         new Handler().postDelayed(new Runnable(){
             @Override
