a68b25d098d4119ffc2b7837f2196de7bc8cb7a8
caraemberger <emberger@cip.ifi.lmu.de>
2020-08-10 23:05:53 +0200

    splash screen update

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/SplashScreen.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/SplashScreen.java
@@ -3,9 +3,11 @@ package de.lmu.treeapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import de.lmu.treeapp.R;
 
@@ -14,6 +16,8 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_splash_screen);
 
 
