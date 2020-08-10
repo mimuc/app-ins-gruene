e6207235916d92b5dd3f0ab38db51e9287f6c9b0
caraemberger <emberger@cip.ifi.lmu.de>
2020-08-10 20:58:48 +0200

    ScrollView and more imprint content
    added further content fot imprint and added scrollbar

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/Imprint.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/Imprint.java
@@ -18,10 +18,22 @@ public class Imprint extends AppCompatActivity {
 
         //Button btn_forward = findViewById(R.id.game_onlyDescription_sendButton);
 
-        TextView imprint_title = findViewById(R.id.impressum_title);
-        TextView imprint_text = findViewById(R.id.impressum_text);
-        TextView publisher_title = findViewById(R.id.herausgeber_title);
-        TextView publisher_text = findViewById(R.id.herausgeber_text);
+        TextView imprint_title = findViewById(R.id.imprint_title);
+        TextView imprint_text = findViewById(R.id.imprint_text);
+        TextView publisher_title = findViewById(R.id.publisher_title);
+        TextView publisher_text = findViewById(R.id.publisher_text);
+        TextView authority_title = findViewById(R.id.authority_title);
+        TextView authority_text = findViewById(R.id.authority_text);
+        TextView tax_id_title = findViewById(R.id.tax_id_title);
+        TextView tax_id_text = findViewById(R.id.tax_id_text);
+        TextView tec_implementation_title = findViewById(R.id.tec_implementation_title);
+        TextView tec_implementation_text = findViewById(R.id.tec_implementation_text);
+        TextView text_title = findViewById(R.id.text_title);
+        TextView text_text = findViewById(R.id.text_text);
+        TextView graphics_title = findViewById(R.id.graphics_title);
+        TextView graphics_text = findViewById(R.id.graphics_text);
+        TextView disclaimer_title = findViewById(R.id.disclaimer_title);
+        TextView disclaimer_text = findViewById(R.id.disclaimer_text);
 
         // Close the last activity on the stack (the imprint) to come back to the tree overview page
         /*
