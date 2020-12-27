2e222491ec95a1f95490a9c43d4d246c340bbe38
hanna <hannadaxer@hotmail.com>
2020-12-27 14:19:25 +0100

    setup

---
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/picturePuzzle/GameActivity_PicturePuzzle.java
@@ -0,0 +1,14 @@
+package de.lmu.treeapp.activities.minigames.picturePuzzle;
+
+import de.lmu.treeapp.R;
+import de.lmu.treeapp.activities.minigames.base.GameActivity_Base;
+
+public class GameActivity_PicturePuzzle extends GameActivity_Base {
+
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_game__picture_puzzle;
+    }
+}
