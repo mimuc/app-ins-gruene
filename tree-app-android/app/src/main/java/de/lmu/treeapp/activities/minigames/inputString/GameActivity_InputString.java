890af7106ef89aa5dd6bed9a64c81a970c167416
Stefan Körner <stefan.koerner@campus.lmu.de>
2021-01-12 18:48:51 +0100

    - Anzeigen welche Bubbles an der falschen Position sind - Am Schluss den vollständigen Satz im Popup anzeigen

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/inputString/GameActivity_InputString.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/inputString/GameActivity_InputString.java
@@ -3,32 +3,42 @@ package de.lmu.treeapp.activities.minigames.inputString;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputEditText;
 
 import de.lmu.treeapp.R;
 import de.lmu.treeapp.activities.minigames.base.GameActivity_Base;
-import de.lmu.treeapp.contentClasses.minigames.Minigame_InputStringAnswer;
+import de.lmu.treeapp.contentData.database.entities.content.GameInputStringRelations;
 
 public class GameActivity_InputString extends GameActivity_Base {
 
-    private Minigame_InputStringAnswer inputStringGame;
+    private GameInputStringRelations inputStringGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        inputStringGame = (Minigame_InputStringAnswer) gameContent;
+        inputStringGame = (GameInputStringRelations) gameContent;
         final TextInputEditText inputField = findViewById(R.id.game_inputString_inputField);
         Button sendButton = findViewById(R.id.game_inputString_sendButton);
 
+        ImageView contentBox = findViewById(R.id.background_image);
+        int backgroundImage = getResources().getIdentifier(inputStringGame.getImageResource(), "drawable", getPackageName());
+        Glide.with(this).load(backgroundImage).into(contentBox);
+
         sendButton.setOnClickListener(view -> {
-            if (inputStringGame.CheckAnswer(inputField.getText().toString())) {
+            if (checkAnswer(inputField.getText().toString())) {
                 onSuccess();
             } else {
                 onFail();
             }
         });
+    }
+
+    private boolean checkAnswer(String toString) {
+        return true;
     }
 
     @Override
