bd30f7ff0bfce8287a6454e9a6d88addf471b650
Sophia Münch <sophia.muench@campus.lmu.de>
2021-02-15 17:30:46 +0100

    Texte umformuliert, Spiele zu den verschiedenen Bäumen hinzugefügt, Foto-Challenge umformuliert und überall hinzugefügt, code cleanup

---
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/onlyDescription/OnlyDescriptionElement.java
@@ -0,0 +1,18 @@
+package de.lmu.treeapp.activities.minigames.onlyDescription;
+
+public class OnlyDescriptionElement {
+
+    private final String text;
+
+    public OnlyDescriptionElement(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setHolder() {
+    }
+
+}
--- tree-app-android/app/src/main/java/de/lmu/treeapp/contentData/database/entities/content/GameOnlyDescriptionRelations.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/contentData/database/entities/content/GameOnlyDescriptionRelations.java
@@ -1,9 +1,32 @@
 package de.lmu.treeapp.contentData.database.entities.content;
 
+import androidx.room.Relation;
+
+import java.util.List;
+
 /**
  * The persistent database for the game OnlyDescription with its related attributes.
  */
 public class GameOnlyDescriptionRelations extends GameRelations {
+
+    @Relation(parentColumn = "id", entityColumn = "gameId")
+    protected List<GameOnlyDescriptionTextItem> textItems;
+
+    @Relation(parentColumn = "id", entityColumn = "gameId")
+    protected List<GameOnlyDescriptionImageItem> imageItems;
+
+    public GameOnlyDescriptionRelations(List<GameOnlyDescriptionTextItem> textItems, List<GameOnlyDescriptionImageItem> imageItems) {
+        this.textItems = textItems;
+        this.imageItems = imageItems;
+    }
+
+    public List<GameOnlyDescriptionTextItem> getTextItems() {
+        return textItems;
+    }
+
+    public List<GameOnlyDescriptionImageItem> getImageItems() {
+        return imageItems;
+    }
 
     // Maybe split into cards, which each have its own description
 }
