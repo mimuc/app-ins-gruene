81a19698236c1f67a09dedf061248e6f2b3e808f
David <d.klein@campus.lmu.de>
2021-03-14 20:36:15 +0100

    fixes
    colors for profile edit changed
    removed overlooked comments

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/About.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/About.java
@@ -1,9 +1,12 @@
 package de.lmu.treeapp.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 import de.lmu.treeapp.R;
 
@@ -16,7 +19,12 @@ public class About extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         TextView about_title = findViewById(R.id.about_title);
+        TextView about_subtitle_creation = findViewById(R.id.about_subtitle_creation);
         TextView about_text = findViewById(R.id.about_text);
+        TextView about_subtitle_intention = findViewById(R.id.about_subtitle_intention);
+        TextView about_intention_text = findViewById(R.id.about_intention_text);
+        TextView about_subtitle_logos = findViewById(R.id.about_subtitle_logos);
+        ImageView about_logos = findViewById(R.id.about_logos);
     }
 
     // Remove the current activity from the stack to switch to the previous one
