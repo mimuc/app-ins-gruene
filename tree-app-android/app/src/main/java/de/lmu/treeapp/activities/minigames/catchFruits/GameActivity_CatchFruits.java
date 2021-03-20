c8b62e443cc2ffd4a38ca4aadc30ab4ec7ddc5ee
Sophia Münch <sophia.muench@campus.lmu.de>
2021-03-20 16:47:08 +0000

    Unified back action on Minigames

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/catchFruits/GameActivity_CatchFruits.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/minigames/catchFruits/GameActivity_CatchFruits.java
@@ -180,7 +180,7 @@ public class GameActivity_CatchFruits extends GameActivity_Base {
 
         btnBack.setOnClickListener(v -> {
             popupGameover.dismiss();
-            if (gameSuccess()) {
+            if (isDone()) {
                 onSuccess();
             } else {
                 onBackPressed();
@@ -189,7 +189,7 @@ public class GameActivity_CatchFruits extends GameActivity_Base {
 
         btnRetry.setOnClickListener(v -> {
             popupGameover.dismiss();
-            if (gameSuccess()) {
+            if (isDone()) {
                 DataManager.getInstance(getApplicationContext()).setGameCompleted(parentCategory, gameContent.getId(), parentTree).subscribe();
             }
             resetGame();
@@ -544,7 +544,7 @@ public class GameActivity_CatchFruits extends GameActivity_Base {
         starImageView5.setImageResource(ids.get(4));
 
 
-        if (curScoreLeaf >= goalLeaf && curScoreFruit >= goalFruit) {
+        if (isDone()) {
             tvEndTitle.setText(R.string.popup_win_title_done);
             btnBack.setText(getResources().getString(R.string.button_done));
             squirrelBar.setImageResource(R.drawable.ic_mascott_true_only_bar);
@@ -639,7 +639,8 @@ public class GameActivity_CatchFruits extends GameActivity_Base {
     /***
      * Check if the user has won the minigame
      * **/
-    private boolean gameSuccess() {
+    @Override
+    protected boolean isDone() {
         return curScoreLeaf >= goalLeaf && curScoreFruit >= goalFruit;
     }
 
@@ -661,18 +662,4 @@ public class GameActivity_CatchFruits extends GameActivity_Base {
         squirrelTail.animate().rotationBy(-10).setDuration(400);
 
     }
-
-    @Override
-    public void onBackPressed() {
-        if (gameSuccess()) onSuccess();
-        else super.onBackPressed();
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        if (gameSuccess()) onSuccess();
-        else super.onSupportNavigateUp();
-        return true;
-    }
-
 }
