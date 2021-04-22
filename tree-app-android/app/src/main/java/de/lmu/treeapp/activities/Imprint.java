61e5e90a78d7bdcd3a0f7401b983abe42f854ce9
Lisa <lisagoertz95@gmx.de>
2021-04-22 13:34:52 +0200

    Update imprint/privacy/about
    Added imprint text, privacy text and names in about section

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/Imprint.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/Imprint.java
@@ -19,16 +19,8 @@ public class Imprint extends AppCompatActivity {
         TextView imprint_text = findViewById(R.id.imprint_text);
         TextView publisher_title = findViewById(R.id.publisher_title);
         TextView publisher_text = findViewById(R.id.publisher_text);
-        TextView authority_title = findViewById(R.id.authority_title);
-        TextView authority_text = findViewById(R.id.authority_text);
-        TextView tax_id_title = findViewById(R.id.tax_id_title);
-        TextView tax_id_text = findViewById(R.id.tax_id_text);
         TextView tec_implementation_title = findViewById(R.id.tec_implementation_title);
         TextView tec_implementation_text = findViewById(R.id.tec_implementation_text);
-        TextView text_title = findViewById(R.id.text_title);
-        TextView text_text = findViewById(R.id.text_text);
-        TextView graphics_title = findViewById(R.id.graphics_title);
-        TextView graphics_text = findViewById(R.id.graphics_text);
         TextView disclaimer_title = findViewById(R.id.disclaimer_title);
         TextView disclaimer_text = findViewById(R.id.disclaimer_text);
         TextView copyright_title = findViewById(R.id.copyright_title);
--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/Privacy.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/Privacy.java
@@ -15,8 +15,13 @@ public class Privacy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy);
 
-        TextView privacy_maintitle = findViewById(R.id.privacy_maintitle);
-        TextView privacy_maintext = findViewById(R.id.privacy_maintext);
+        TextView privacy_title = findViewById(R.id.privacy_title);
+        TextView publisher_title = findViewById(R.id.publisher_title);
+        TextView publisher_text = findViewById(R.id.publisher_text);
+        TextView access_title = findViewById(R.id.privacy_access_title);
+        TextView access_text = findViewById(R.id.privacy_access_text);
+        TextView data_title = findViewById(R.id.privacy_data_title);
+        TextView data_text = findViewById(R.id.privacy_data_text);
     }
 
     // Remove the current activity from the stack to switch to the previous one
