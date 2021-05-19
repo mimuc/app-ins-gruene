9439e22dcb4159e767645a3bb6b136deebaab121
Gustl22 <git@reb0.org>
2021-05-19 13:15:54 +0200

    Improvements for Imprint, About, Privacy

--- tree-app-android/app/src/main/java/de/lmu/treeapp/activities/About.java
+++ tree-app-android/app/src/main/java/de/lmu/treeapp/activities/About.java
@@ -1,22 +1,23 @@
 package de.lmu.treeapp.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
-
+import android.view.View;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
-
 import de.lmu.treeapp.R;
 
-public class About extends AppCompatActivity {
+public class About extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.menu_about_us);
         setContentView(R.layout.activity_about);
+
+        LinearLayout layout = findViewById(R.id.open_source_wrapper);
+        layout.setOnClickListener(this);
     }
 
     // Remove the current activity from the stack to switch to the previous one
@@ -31,5 +32,14 @@ public class About extends AppCompatActivity {
     public void onBackPressed() {
         finish();
         super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.open_source_wrapper) {
+            Uri uri = Uri.parse(getString(R.string.open_source_link));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
     }
 }
